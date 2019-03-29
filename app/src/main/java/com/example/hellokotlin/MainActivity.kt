ace6bb871c9db890119f421c2dff8432310d9453
mvkoroteev <mvkoroteev@fa.ru>
2019-03-29 13:28:27 +0300

    List adapter added

--- app/src/main/java/com/example/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/example/hellokotlin/MainActivity.kt
@@ -2,9 +2,15 @@ package com.example.hellokotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.ArrayList
+
 
 class MainActivity : AppCompatActivity() {
+
+    internal var mNameList = ArrayList<String>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,8 +18,13 @@ class MainActivity : AppCompatActivity() {
 
         main_textview.text = "Hello from Kotlin!"
 
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, mNameList)
+        main_listview.adapter = adapter
+
         main_button.setOnClickListener {
-            main_textview.text = "Button is pressed"
+            // main_textview.text = "Button is pressed"
+            mNameList.add(main_edittext.text.toString())
+            adapter.notifyDataSetChanged()
         }
     }
 }
